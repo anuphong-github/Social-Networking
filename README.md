496b6ddb7944603207e236c1f83789e3b7c81f6a
Anuphongkrystal <Anuphongkrystal@outlook.com>
2020-09-05 23:21:00 -0700

    Add Logout

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Social-Networking
 
 ### To do list :
-[x]  <br>
+[x] ทำหน้า Logout 
+[x] ทำหน้า home.html ก่อน
+[x] ลุยเขียนฟังก์ชั่นต่อ ของ Blog app  <br>
+
 ### Doing :
 [+] <br>
 [+] <br>
+
 ### Bug fix:
 
 ### Bug created :
--- SocialNetworking/urls.py
+++ SocialNetworking/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/',users_app_views.register,name='register-users'),
     path('profile/', users_app_views.profile, name='profile'),
 ]
